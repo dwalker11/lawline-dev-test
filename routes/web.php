3240063257c7de134ce3d342a3d937aacab23366
Devon Walker <devon@winterfell.localdoamin>
2017-09-23 09:10:55 -0400

    Added basic authentication

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,15 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/products', 'ProductController@index');
-$router->get('/products/{id}', 'ProductController@show');
-$router->post('/products', 'ProductController@store');
-$router->put('/products/{id}', 'ProductController@update');
-$router->delete('/products/{id}', 'ProductController@destroy');
-$router->get('/products/user/{user}', 'ProductController@showUserProducts');
-$router->put('/products/{id}/user/{user}', 'ProductController@associateUser');
-$router->put('/products/{id}/user', 'ProductController@dissociateUser');
+$router->group(['middleware' => 'auth:api'], function () use ($router) {
+    $router->get('/products', 'ProductController@index');
+    $router->get('/products/{id}', 'ProductController@show');
+    $router->post('/products', 'ProductController@store');
+    $router->put('/products/{id}', 'ProductController@update');
+    $router->delete('/products/{id}', 'ProductController@destroy');
+
+    $router->get('/products/user/{user}', 'ProductController@showUserProducts');
+    $router->put('/products/{id}/user/{user}', 'ProductController@associateUser');
+    $router->put('/products/{id}/user', 'ProductController@dissociateUser');
+    $router->put('/products/{id}/image', 'ProductController@uploadProductImage');
+});
